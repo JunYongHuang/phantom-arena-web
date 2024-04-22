4a34d7f190c139cb07f727343abf83e2e9603484
hjy <545925654@qq.com>
2024-04-22 11:15:01 +0800

    111

---
+++ web/js/custom/css/tongji.js
@@ -0,0 +1,18 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: hjy
+ * @Date: 2024-04-09 13:06:24
+ * @LastEditors: hjy
+ * @LastEditTime: 2024-04-09 13:07:03
+ * @Description: 
+ */
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c400152bc72737aca6498bb206304940";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
