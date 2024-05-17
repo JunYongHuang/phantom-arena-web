29b3aea77b92e968207a39f539782f937f711d09
hjy <545925654@qq.com>
2024-05-18 00:55:37 +0800

    2222

--- web/js/Config-6de44.js
+++ web/js/Config-6de44.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2024-05-17 22:27:21
+ * @LastEditTime: 2024-05-17 23:14:17
  */
 
 
-var game_version = "20240517_03";
+var game_version = "20240518_01";
 
 var v_type = 2; //0=QA；1=S0; 2=SN;
 
