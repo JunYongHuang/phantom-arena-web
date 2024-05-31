42cb5b5d1ca1a3ded0400e05a2677553a343370e
hjy <545925654@qq.com>
2024-05-31 14:22:55 +0800

    1111

--- web/js/Config-0b9db.js
+++ web/js/Config-0b9db.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2024-05-30 23:25:14
+ * @LastEditTime: 2024-05-30 23:54:46
  */
 
 
-var game_version = "V_0530_03";	
+var game_version = "V_0531_07";	
 
 var v_type = 2; //0=QA；1=S0; 2=SN;
 
