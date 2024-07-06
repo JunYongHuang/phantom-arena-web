b16d9899afce224830560722ce32758198b76120
hjy <545925654@qq.com>
2024-07-07 00:44:17 +0800

    111

--- web/js/Config-0dbd5.js
+++ web/js/Config-0dbd5.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2024-07-06 13:40:19
+ * @LastEditTime: 2024-07-07 00:37:44
  */
 
 
-var game_version = "V_0706_06";	
+var game_version = "V_0706_08";	
 
 var v_type = 2; //0=QA；1=S0; 2=SN;
 
