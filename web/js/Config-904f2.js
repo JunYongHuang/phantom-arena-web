0ffd0eb953d4c071adaa7f9ec7239ce78d6f4e95
hjy <545925654@qq.com>
2024-07-08 13:52:26 +0800

    11111

--- web/js/Config-904f2.js
+++ web/js/Config-904f2.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2024-07-07 00:37:44
+ * @LastEditTime: 2024-07-08 13:48:50
  */
 
 
-var game_version = "V_0706_08";	
+var game_version = "V_0708_01";	
 
 var v_type = 2; //0=QA；1=S0; 2=SN;
 
