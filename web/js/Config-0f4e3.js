7396ce6602ec82981c08d2abd5b36981d27be1ea
hjy <545925654@qq.com>
2024-07-16 14:49:24 +0800

    1111

--- web/js/Config-0f4e3.js
+++ web/js/Config-0f4e3.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2024-07-15 21:02:51
+ * @LastEditTime: 2024-07-16 14:42:20
  */
 
 
-var game_version = "V_0715_02";	
+var game_version = "V_0716_02";	
 
 var v_type = 2; //0=QA；1=S0; 2=SN;
 
