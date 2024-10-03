5810eaa2d790bd08dbc384f76859a93dbce1bdd6
hjy <545925654@qq.com>
2024-10-03 22:55:04 +0800

    11111

--- web/js/Config-bfa5e.js
+++ web/js/Config-bfa5e.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2024-10-01 21:10:21
+ * @LastEditTime: 2024-10-01 22:51:05
  */
 
 
-var game_version = "V_1001_01";
+var game_version = "V_1003_01";
 
 var v_type = 2; //0=QA；1=S0; 2=SN;
 
