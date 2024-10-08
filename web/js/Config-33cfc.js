74b9b47b559c29efc5d66c0ac768c1ce7f3de004
hjy <545925654@qq.com>
2024-10-08 23:06:52 +0800

    111111

--- web/js/Config-33cfc.js
+++ web/js/Config-33cfc.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2024-10-05 03:29:49
+ * @LastEditTime: 2024-10-08 23:01:32
  */
 
 
-var game_version = "V_1005_01";
+var game_version = "V_1008_02";
 
 var v_type = 2; //0=QA；1=S0; 2=SN;
 
