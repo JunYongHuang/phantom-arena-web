2f1c4fe9e56a3b15f40f1653d7330a829060b350
hjy <545925654@qq.com>
2024-12-05 22:03:50 +0800

    1111

--- web/js/Config-51593.js
+++ web/js/Config-51593.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2024-12-03 23:37:59
+ * @LastEditTime: 2024-12-04 16:08:31
  */
 
 
-var game_version = "V_1204_02";
+var game_version = "V_1205_02";
 
 var v_type = 2; //0=QA；1=S0; 2=SN;
 
