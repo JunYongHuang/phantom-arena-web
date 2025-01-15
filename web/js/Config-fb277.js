e37cdf86e77accdc092f8ff06ac385e0fea4ad70
hjy <545925654@qq.com>
2025-01-15 16:55:38 +0800

    11111

--- web/js/Config-fb277.js
+++ web/js/Config-fb277.js
@@ -4,11 +4,11 @@
  * @Author: hjy
  * @Date: 2023-12-21 10:15:05
  * @LastEditors: hjy
- * @LastEditTime: 2025-01-13 13:31:56
+ * @LastEditTime: 2025-01-15 16:43:27
  */
 
 
-var game_version = "V_0114_01";
+var game_version = "V_0115_01";
 
 var v_type = 2; //0=QA；1=S0; 2=SN;
 
